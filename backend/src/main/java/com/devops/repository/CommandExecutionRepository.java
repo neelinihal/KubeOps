1320d2d7f243a8fea7979a71ef3b635abdcb1e2f
Nihal Neeli <95561241+neelinihal@users.noreply.github.com>
2026-05-02 19:51:51 +0530

    Delete backend directory

--- backend/src/main/java/com/devops/repository/CommandExecutionRepository.java
+++
@@ -1,9 +0,0 @@
-package com.devops.repository;
-
-import com.devops.model.CommandExecution;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
-
-public interface CommandExecutionRepository extends JpaRepository<CommandExecution, Long> {
-    List<CommandExecution> findAllByOrderByExecutedAtDesc();
-}
